b8ff096de7e6073336a163a01f580fe3d19e6f63
niklai99 <lai.nicolo99@gmail.com>
2022-01-14 23:38:52 +0100

    updating nplm nn input handling

--- nplm/nl/ModelBuilder.py
+++ nplm/nl/ModelBuilder.py
@@ -1,94 +1,73 @@
-import keras.backend as K
-from keras.constraints import Constraint
-from keras.models import Model
-from keras.layers import Dense, Activation, Input, Layer, BatchNormalization            
-
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Activation, Input, Layer
+from tensorflow.keras.constraints import Constraint
 
 
 class WeightClip(Constraint):
-    '''Clips the weights incident to each hidden unit to be inside a range'''
+    """Clips the weights incident to each hidden unit to be inside a range"""
     
     def __init__(self, c=2):
         self.c = c
         
     def __call__(self, p):
-        return K.clip(p, -self.c, self.c)
+        return tf.clip_by_value(p, clip_value_min=-self.c, clip_value_max=self.c)
     
     def get_config(self):
         return {'name': self.__class__.__name__, 'c': self.c}
 
 
-class ModelBuilder:
-    '''Builds the NN architecture'''
+class ModelBuilder(Model):
+    """Neural network builder"""
     
     def __init__(self, 
-                 n_input: int, 
-                 latentsize: int = 3, layers: int = 1, weight_clipping: float = 7., 
-                 internal_activation: str = 'tanh',
-                 batch_norm_bool: bool = False,
-                 more_batch_norm_bool: bool = False,
-                 custom_activation_bool: bool = False,
-                 custom_const: float = 1.
+                 input_shape: tuple, 
+                 architecture: list[int] = [1, 3, 1],
+                 weight_clipping: float = 7., 
+                 activation: str = "tanh",
+                 name: str = None, 
+                 **kwargs
                 ):
-        '''initialize NN parameters and user choices'''
+        """
+        Initializes NN hyperparameters
+
+        Args:
+            n_input (tuple): shape of the input dataset
+            architecture (list, optional): architecture of the neural network. Defaults to [1, 3, 1].
+            weight_clipping (float, optional): weight clipping contraint. Defaults to 7.
+            internal_activation (str, optional): activation function. Defaults to "tanh".
+            custom_activation_bool (bool, optional): custom activation function. Defaults to False.
+            custom_const (float, optional): custom activation function parameter. Defaults to 1.
+            name (str, optional): name of the model. Defaults to None.
+        """
+        super().__init__(name=name, **kwargs)
+
+        self.inputs = Input(
+            shape=input_shape
+        )
         
-        self.n_input = n_input
-        self.latentsize = latentsize
-        self.layers = layers
-        self.weight_clipping = weight_clipping
-        self.internal_activation = internal_activation
-        self.batch_norm_bool = batch_norm_bool
-        self.more_batch_norm_bool = more_batch_norm_bool
-        self.custom_activation_bool = custom_activation_bool
-        self.custom_const = custom_const
+        self.hidden_layers = [
+            Dense(
+                    architecture[i+1], 
+                    input_shape=(architecture[i],), 
+                    activation=activation,
+                    kernel_constraint = WeightClip(weight_clipping)
+                ) 
+            for i in range(len(architecture)-2)
+        ]
+        
+        self.output_layer = Dense(
+            architecture[-1],
+            input_shape=(architecture[-2],), 
+            activation="linear",
+            kernel_constraint = WeightClip(weight_clipping)
+        )
     
     
-    def __call__(self):
-        '''call method: builds the NN'''
+        def call(self, x):
+            for hidden_layer in self.hidden_layers:
+                x = hidden_layer(x)
+            x = self.output_layer(x)
+            return x
         
-        def custom_activation(x):
-            '''custom activation function for the output layer'''
-            return self.custom_const*((K.sigmoid(x/self.custom_const) * 2) - 1) 
-        
-        inputs = Input( shape=(self.n_input, ) )
-        
-        dense  = Dense(
-                        self.latentsize, input_shape=(self.n_input, ), 
-                        activation=self.internal_activation, 
-                        kernel_constraint = WeightClip(self.weight_clipping)
-                )(inputs)
-        
-        if self.batch_norm_bool:
-            dense = BatchNormalization()(dense)
-            
-        for l in range(self.layers-1):
-            dense  = Dense(
-                            self.latentsize, input_shape=(self.latentsize, ), 
-                            activation=self.internal_activation, 
-                            kernel_constraint = WeightClip(self.weight_clipping)
-                    )(dense)
-            
-            if self.more_batch_norm_bool:
-                dense = BatchNormalization()(dense)
-#                 if l == 1:
-#                     dense = BatchNormalization()(dense)
-        
-        
-        if self.custom_activation_bool:
-            output = Dense(
-                        1, input_shape=(self.latentsize,), 
-                        activation=custom_activation, 
-                        kernel_constraint = WeightClip(self.weight_clipping)
-                )(dense) 
-        elif not self.custom_activation_bool:
-            output = Dense(
-                        1, input_shape=(self.latentsize,), 
-                        activation='linear', 
-                        kernel_constraint = WeightClip(self.weight_clipping)
-                )(dense)
-            
-        
-        model = Model(inputs=[inputs], outputs=[output])
-        
-        return model
         
