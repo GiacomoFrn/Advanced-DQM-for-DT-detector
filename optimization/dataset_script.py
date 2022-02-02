b45f80329cf502d77e4fcdfe6689c748bb81f177
c0pp1 <albertocoppi@outlook.it>
2022-02-02 10:41:50 +0100

    added optimization/

---
+++ optimization/dataset_script.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import numpy as np
+import yaml
+import math
+import warnings
+import argparse
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
+warnings.filterwarnings("ignore")
+
+
+from eventsFactory import getEvents
+from reco import getRecoResults
+
+"""USAGE:
+
+    python dataset_script.py -i <input_data_directory> -o <output_data_directory> -c <config_directory> -run <last_4_digits_of_run>
+    
+    The I/O directories should be ../data/ (as default)
+    
+    The configuration directories should be ../config/ (as default)
+    
+    NOTE that data and config files should be named accordingly
+    
+    EXAMPLE: 
+    
+    python dataset_script.py -run 0054 
+    
+"""
+
+# CONSTANTS
+USE_TRIGGER = False
+RUN_TIME_SHIFT = 0
+KEEP = ["FPGA", "TDC_CHANNEL", "HIT_DRIFT_TIME", "D_WIRE_HIT", "m"]
+
+
+def argParser():
+    """manages command line arguments"""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--input", type=str, default="../data/", help="input data directory"
+    )
+    parser.add_argument(
+        "-o", "--output", type=str, default="../data/", help="output data directory"
+    )
+    parser.add_argument(
+        "-c", "--config", type=str, default="../config/", help="config directory"
+    )
+    parser.add_argument("-run", "--run", type=str, default="0054", help="run number")
+
+    return parser.parse_args()
+
+
+def buildDataframe(stream_df, cfg):
+
+    df = pd.DataFrame()
+
+    # reco (da sistemare getEvents e getRecoResults)
+    print("Getting events...")
+    events = getEvents(stream_df, cfg, RUN_TIME_SHIFT, USE_TRIGGER)
+    print("Reconstructing tracks...")
+    resultsList, resultsData, resultsHits, resultsDf = getRecoResults(
+        events, USE_TRIGGER
+    )
+    print("Building dataframe...")
+    # out df
+    for df_ in resultsDf:
+        df_["D_WIRE_HIT"]= df_["X"]-df_["WIRE_X_GLOB"]
+        df_ = df_[KEEP]
+        df = pd.concat([df, df_], axis=0, ignore_index=True)
+
+    # add a sequential channel tag
+    df.loc[(df["FPGA"] == 0), "CH"] = df["TDC_CHANNEL"]
+    df.loc[(df["FPGA"] == 1), "CH"] = df["TDC_CHANNEL"] + 128
+    df_ = df.drop(["FPGA", "TDC_CHANNEL"], axis=1)
+    df_["CH"] = df_["CH"].astype(np.uint32)
+
+    # clean dataset
+    df = df_[["CH", "HIT_DRIFT_TIME",'D_WIRE_HIT', "m"]]
+    df = df[(df["HIT_DRIFT_TIME"] > -200) & (df["HIT_DRIFT_TIME"] < 600)]
+    df = df[(df['D_WIRE_HIT'] > -21) & (df['D_WIRE_HIT'] < 21)]
+
+    # rad to deg conversion
+    df["THETA"] = np.arctan(df["m"]) * 180.0 / math.pi
+
+    print("Dataframe ready!")
+
+    return df
+
+
+def saveChannels(df, OUTPUT_PATH, RUNNUMBER):
+
+    FILE_NAME = f"RUN00{RUNNUMBER}_channels.h5"
+    save_to = OUTPUT_PATH + FILE_NAME
+
+    print("Saving data...")
+    channels = []
+    for channel in np.unique(df["CH"]):
+        channels.append(df[df["CH"] == channel])
+        df[df["CH"] == channel].to_hdf(save_to, key=f"ch{channel}", mode="a")
+
+    return channels
+
+
+def main(args):
+
+    # store command line arguments
+    DATA_PATH = args.input
+    CONFIG_PATH = args.config
+    RUNNUMBER = args.run
+    OUTPUT_PATH = args.output
+
+    # link data and config files
+    data_file = DATA_PATH + f"RUN00{RUNNUMBER}_data.txt"
+    config_file = CONFIG_PATH + f"RUN00{RUNNUMBER}.yml"
+
+    # read data from file
+    print("Reading data from file...")
+    stream_df = pd.read_csv(data_file)
+    # read config from file
+    print("Reading config from file...")
+    with open(config_file, "r") as f:
+        cfg = yaml.safe_load(f)
+
+    df = buildDataframe(stream_df, cfg)
+    channels = saveChannels(df, OUTPUT_PATH, RUNNUMBER)
+
+    return
+
+
+if __name__ == "__main__":
+    args = argParser()
+    main(args)
---
+++ optimization/run_script.py
@@ -0,0 +1,95 @@
+import os
+import argparse
+import os.path
+import time
+
+"""
+CMS CLUSTER ONLY!!!!
+
+Python script that uses condor to exploit the CPU cluster (available only through t2-ui-12 machine via ssh) to distribute jobs to different computing resources.
+
+USAGE:
+
+python run_script.py -o output_data_directory -n number_of_files_to_read -p python_script_file
+
+
+Example: 
+
+python run_script.py -i ../data/ -o ../data/ -c ../config/ -run 1231 -p dataset_script.py
+
+CONSIDER SETTING UP MEANINGFUL DEFAULT VALUES SO THAT IT BECOMES
+
+python run_condor_script.py
+
+"""
+
+
+def argParser():
+    """manages command line arguments"""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--input", type=str, default="../data/", help="input data directory"
+    )
+    parser.add_argument(
+        "-o", "--output", type=str, default="../data/", help="output data directory"
+    )
+    parser.add_argument(
+        "-c", "--config", type=str, default="../config/", help="config directory"
+    )
+    parser.add_argument("-run", "--run", type=int, default=1231, help="run number")
+    parser.add_argument("-p"  , "--pyscript", type=str, default="./dataset_script.py", help="python script to execute")
+
+    return parser.parse_args()
+
+
+def main(args):
+    """
+        MAIN FUNCTION gets data from CloudVeneto and save them into a .txt file
+        
+        args: the command line arguments  
+    """
+    
+    INPUT_PATH    = args.input
+    OUTPUT_PATH   = args.output
+    CONFIG_PATH   = args.config
+    RUN           = args.run
+    PYTHON_SCRIPT = args.pyscript
+    
+    
+    job_folder = "condor"+str(time.time())
+    condor_path = "./condor_jobs/"
+    job_path = condor_path+job_folder
+    if not os.path.exists(condor_path):
+        os.makedirs(condor_path)
+    os.makedirs(job_path)
+    
+    
+    joblabel=f"RUN{RUN}"
+    
+    # src file
+    with open(f"{job_path}/{joblabel}.src" , "w") as script_src:
+        script_src.write("#!/bin/bash\n")
+        script_src.write('eval "$(/lustre/cmswork/nlai/anaconda/bin/conda shell.bash hook)" \n')
+        script_src.write(f"python {os.getcwd()}/{PYTHON_SCRIPT} -i {INPUT_PATH} -o {OUTPUT_PATH} -c {CONFIG_PATH} -run {RUN}") 
+    os.system(f"chmod a+x {job_path}/{joblabel}.src") # THIS MAKES THE FILE EXECUTABLE
+    
+    # condor file
+    with open(f"{job_path}/{joblabel}.condor", "w") as script_condor:
+        script_condor.write(f"executable = {job_path}/{joblabel}.src\n" )
+        script_condor.write("universe = vanilla\n")
+        script_condor.write(f"output = {job_path}/{joblabel}.out\n" )
+        script_condor.write(f"error =  {job_path}/{joblabel}.err\n" )
+        script_condor.write(f"log = {job_path}/{joblabel}.log\n")
+        script_condor.write("+MaxRuntime = 500000\n")
+        script_condor.write("queue\n")
+        
+    # condor file submission
+    os.system(f"condor_submit {job_path}/{joblabel}.condor") 
+    
+    return
+
+
+if __name__ == "__main__":
+    args = argParser()
+    main(args)
